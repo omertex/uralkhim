95a217911ba777d04c4b95b6ed3a888105a259ab
avanushin <avanushin@omertex.com>
2020-01-29 19:18:08 +0300

    fix aside menu scroll

--- src/shared_components/Aside/Aside.styled.js
+++ src/shared_components/Aside/Aside.styled.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import CleanButton from "../CleanButton/CleanButton";
+import styled from 'styled-components';
+import CleanButton from '../CleanButton/CleanButton';
 
 export const Aside = styled.div`
   position: relative;
@@ -8,18 +8,18 @@ export const Aside = styled.div`
   background-color: #fff;
   margin-left: auto;
   transition: 0.25s;
-  margin-right: ${({state}) => (state === "entering" || state === "entered" ? 0 : -500)}px;
+  margin-right: ${({state}) => (state === 'entering' || state === 'entered' ? 0 : -500)}px;
   overflow-y: auto;
 `;
 
 export const Overlay = styled.div`
-  position: absolute;
+  position: fixed;
   right: 0;
   top: 0;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.3);
   z-index: 1;
-  width: ${({state}) => state === "exited" ? 0 : '100%'};
+  width: ${({state}) => state === 'exited' ? 0 : '100%'};
 `;
 
 export const CloseBtn = styled(CleanButton)`
